3236f324dc59a304566861510f7bb3ff537c466a
YunHo <shdbsghxg@gmail.com>
2018-02-01 12:22:18 +0900

    write templates/blog/post_list.html file, return rendered html at from blog.view.post_list

--- django/blog/views.py
+++ django/blog/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def post_list(request):
@@ -9,8 +10,12 @@ def post_list(request):
     # 5. config.urls module delivers every requests except ''(admin/) to blog.urls module
     # 6. blog.urls looks for whether there's the same pattern with received requests
     # 7. if it is, execute connected function(view)
+    #   7-1. from TEMPLATES attribute inside settings module, search DIRS list for finding blog/post_list.html
+    #   7-2. render() found html(blog/post_list.html) and return it
     # 8. deliver the return value of function(view) to the browser
 
-
     # return text-data response as HTTP protocol
-    return HttpResponse('<html><body><h1>post list</h1><p>Post list will be here</p></body></html>')
+    # return HttpResponse('<html><body><h1>post list</h1><p>Post list will be here</p></body></html>')
+
+    # return 'blog/post_list.html' template file as HTTP protocol
+    return render(request, 'blog/post_list.html')
